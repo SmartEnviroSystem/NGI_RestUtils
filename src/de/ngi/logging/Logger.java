51033463ebfcc57261880b4937d586d2b46801ac
Florian Fehring <Florian Fehring@DESKTOP-SKAFEOK.dhcp.fh-bielefeld.de>
2025-07-14 10:59:36 +0200

    Fixed: Logger: Error on closeing logging process when no process was started

--- src/de/ngi/logging/Logger.java
+++ src/de/ngi/logging/Logger.java
@@ -13,51 +13,55 @@ public class Logger {
     public static void startProcess() {
         startProcess(UUID.randomUUID().toString());
     }
-    
+
     public static void startProcess(String processId) {
         PROCESS_ID.set(processId);
-        log("Started process: " + processId,4);
+        log("Started process: " + processId, 4);
     }
 
     public static void log() {
-        log(null,3);
+        log(null, 3);
     }
-    
+
     public static void log(String message) {
         // stackDepth = 3 => Caller of this method. Method calls: Caller -> log(message) -> log(message, stackDepth) -> getStackTrace()
         log(message, 3);
     }
-    
-    public static void log(String message, int stackDepth) {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
 
-        // Index 0 ist getStackTrace(), Index 1 ist diese Methode selbst, Index 2 ist der Aufrufer
-        if (stackTrace.length > 3) {
-            LoggerEntry logEntry = new LoggerEntry(
-                    PROCESS_ID.get(),
-                    message,
-                    stackTrace[stackDepth].getClassName(),
-                    stackTrace[stackDepth].getMethodName(),
-                    stackTrace[stackDepth].getLineNumber()
-            );
-            LOG_ENTRIES.get().add(logEntry);
-            logConsumers.forEach(consumer -> consumer.accept(logEntry.toString()));
+    public static void log(String message, int stackDepth) {
+        if (PROCESS_ID.get() != null) {
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+            // Index 0 ist getStackTrace(), Index 1 ist diese Methode selbst, Index 2 ist der Aufrufer
+            if (stackTrace.length > 3) {
+                LoggerEntry logEntry = new LoggerEntry(
+                        PROCESS_ID.get(),
+                        message,
+                        stackTrace[stackDepth].getClassName(),
+                        stackTrace[stackDepth].getMethodName(),
+                        stackTrace[stackDepth].getLineNumber()
+                );
+                LOG_ENTRIES.get().add(logEntry);
+                logConsumers.forEach(consumer -> consumer.accept(logEntry.toString()));
+            }
         }
     }
 
     public static void endProcess() {
-        log("Process finished: " + PROCESS_ID.get());
-        System.out.println("Logged for process " + PROCESS_ID.get() + ":");
-        JsonArrayBuilder jab = Json.createArrayBuilder();
-        for(LoggerEntry curEntry : LOG_ENTRIES.get()) {
-            jab.add(curEntry.toJson());
-        }
-        
-        System.out.println(jab.build().toString());
+        if (PROCESS_ID.get() != null) {
+            log("Process finished: " + PROCESS_ID.get());
+            System.out.println("Logged for process " + PROCESS_ID.get() + ":");
+            JsonArrayBuilder jab = Json.createArrayBuilder();
+            for (LoggerEntry curEntry : LOG_ENTRIES.get()) {
+                jab.add(curEntry.toJson());
+            }
 
-        // Bereinige ThreadLocals
-        PROCESS_ID.remove();
-        LOG_ENTRIES.remove();
+            System.out.println(jab.build().toString());
+
+            // Bereinige ThreadLocals
+            PROCESS_ID.remove();
+            LOG_ENTRIES.remove();
+        }
     }
 
     public static void addLogConsumer(Consumer<String> consumer) {
