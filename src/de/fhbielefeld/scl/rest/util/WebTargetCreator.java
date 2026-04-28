545ed18b6c91144e9264a880e04f9f2a708bd217
Florian Fehring <Florian Fehring@DESKTOP-SKAFEOK.dhcp.fh-bielefeld.de>
2026-04-28 12:48:33 +0200

    Added: Possibility to create WebTargets without SSL verification

--- src/de/fhbielefeld/scl/rest/util/WebTargetCreator.java
+++ src/de/fhbielefeld/scl/rest/util/WebTargetCreator.java
@@ -10,10 +10,13 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 //import org.glassfish.jersey.client.filter.EncodingFilter;
 //import org.glassfish.jersey.message.GZipEncoder;
-
 /**
  * Creates web targets baseing on the base_uri for all REST webservices
  *
@@ -70,9 +73,17 @@ public class WebTargetCreator {
      * @param resource Name of the resource where a webtarget should state
      * @return WebTarget to the given resource
      */
-    public WebTarget createWebTarget(String resource) {
+    public WebTarget createWebTarget(String resource, boolean insecure) {
         WebTarget webTarget;
-        Client client = ClientBuilder.newClient();
+        Client client;
+        if (insecure) {
+            client = ClientBuilder.newBuilder()
+                    .sslContext(createInsecureSSLContext())
+                    .hostnameVerifier(insecureHostnameVerifier())
+                    .build();
+        } else {
+            client = ClientBuilder.newClient();
+        }
 //        client.register(MultiPartFeature.class);
 //        client.register(EncodingFilter.class);
 //        client.register(GZipEncoder.class);
@@ -82,8 +93,7 @@ public class WebTargetCreator {
     }
 
     /**
-     * Creates a webtarget to the given resource useing the
-     * localhost:8080/SmartMonitoringBackend/ as base
+     * Creates a webtarget to the given resource
      *
      * @param base_url Servers URL where the REST Webservice is located on
      * @param resource Name of the resource
@@ -91,6 +101,48 @@ public class WebTargetCreator {
      */
     public static WebTarget createWebTarget(String base_url, String resource) {
         WebTargetCreator creator = new WebTargetCreator(base_url);
-        return creator.createWebTarget(resource);
+        return creator.createWebTarget(resource, false);
+    }
+    
+    /**
+     * Creates a webtarget to the given resource with possibility to use a 
+     * unsecure connection.
+     *
+     * @param base_url Servers URL where the REST Webservice is located on
+     * @param resource Name of the resource
+     * @return WebTarget to the resource
+     */
+    public static WebTarget createWebTarget(String base_url, String resource, boolean insecure) {
+        WebTargetCreator creator = new WebTargetCreator(base_url);
+        return creator.createWebTarget(resource, insecure);
+    }
+
+    private static SSLContext createInsecureSSLContext() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new java.security.cert.X509Certificate[0];
+                    }
+
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            return sslContext;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static HostnameVerifier insecureHostnameVerifier() {
+        return (hostname, session) -> true;
     }
 }
